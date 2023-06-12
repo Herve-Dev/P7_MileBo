8c75b8a7468c1a1b2fb0e8f4ca16beb2c32883a3
Hervé Taillefond <madinina972represent@gmail.com>
2023-06-12 23:26:02 +0200

    add entity smartphone with fixtures

---
+++ src/Entity/Smartphone.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SmartphoneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SmartphoneRepository::class)]
+class Smartphone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone_brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone_model = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $phone_description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $phone_created_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhoneBrand(): ?string
+    {
+        return $this->phone_brand;
+    }
+
+    public function setPhoneBrand(string $phone_brand): static
+    {
+        $this->phone_brand = $phone_brand;
+
+        return $this;
+    }
+
+    public function getPhoneModel(): ?string
+    {
+        return $this->phone_model;
+    }
+
+    public function setPhoneModel(string $phone_model): static
+    {
+        $this->phone_model = $phone_model;
+
+        return $this;
+    }
+
+    public function getPhoneDescription(): ?string
+    {
+        return $this->phone_description;
+    }
+
+    public function setPhoneDescription(string $phone_description): static
+    {
+        $this->phone_description = $phone_description;
+
+        return $this;
+    }
+
+    public function getPhoneCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->phone_created_at;
+    }
+
+    public function setPhoneCreatedAt(\DateTimeImmutable $phone_created_at): static
+    {
+        $this->phone_created_at = $phone_created_at;
+
+        return $this;
+    }
+}
