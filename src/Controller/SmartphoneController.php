cfd0dce07edf7d43be691cad53c176d52a6f390e
Hervé Taillefond <madinina972represent@gmail.com>
2023-06-15 14:54:01 +0200

    add route update

--- src/Controller/SmartphoneController.php
+++ src/Controller/SmartphoneController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class SmartphoneController extends AbstractController
 {
@@ -67,5 +68,24 @@ class SmartphoneController extends AbstractController
 
         //Je renvoi ma variable location dans ma jsonResponse
         return new JsonResponse($jsonSmartphone, Response::HTTP_CREATED, ['location' => $location], true);
+    }
+
+    #[Route('/api/smartphone/{id}', name: 'app_smartphone_update', methods: ['PUT'])]
+    public function updateSmartphone(Request $request ,Smartphone $currentSmartphone, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        //AbstractNormalizer fonction pour ecrire dans la donnée récupérée 
+        $updateSmartphone = $serializer->deserialize($request->getContent(), Smartphone::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentSmartphone]);
+
+        //Je vais chercher mon entité society Milbo pour le lier a mon nouveau smartphone ajouter 
+        $society = $em->getReference(Society::class, 1);
+
+        //Je le stock dans mon setSociety 
+        $updateSmartphone->setSociety($society);
+
+        $em->persist($updateSmartphone);
+        $em->flush();
+
+        //Je renvoi ma variable location dans ma jsonResponse
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
